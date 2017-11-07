21222796fb97a6eeb881f8f37cddd50eb8cffbde
Matthew D. Scholefield <matthew3311999@gmail.com>
2017-11-06 21:03:10 -0600

    Preserve filename Apparently this is important to pip

--- setup.sh
+++ setup.sh
@@ -21,8 +21,8 @@ git fetch && git reset --hard origin/rnn
 arch="$(python3 -c 'import platform; print(platform.machine())')"
 
 if ! python3 -c 'import tensorflow' 2>/dev/null && [ "$arch" = "armv7l" ]; then
-    wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/releases/download/v1.1.0/tensorflow-1.1.0-cp34-cp34m-linux_armv7l.whl -O tensorflow.whl
-	sudo pip3 install tensorflow.whl
+    wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/releases/download/v1.1.0/tensorflow-1.1.0-cp34-cp34m-linux_armv7l.whl
+	sudo pip3 install tensorflow-1.1.0-cp34-cp34m-linux_armv7l.whl
 	sudo pip3 uninstall mock || true
     sudo pip3 install mock
 fi
