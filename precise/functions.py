09c3ad927b100bce09071b95efeab4e554237061
Matthew D. Scholefield <matthew3311999@gmail.com>
2018-02-28 20:49:34 -0600

    Change license to Apache 2.0

--- precise/functions.py
+++ precise/functions.py
@@ -1,4 +1,16 @@
-# Copyright (c) 2017 Mycroft AI Inc.
+# Copyright 2018 Mycroft AI Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from typing import *
 
 
@@ -9,7 +21,7 @@ def weighted_log_loss(yt, yp) -> Any:
     yp: Prediction
     """
     from keras import backend as K
-    weight = 0.99  # [0..1] where 1 is inf bias
+    weight = 0.5  # [0..1] where 1 is inf bias
 
     pos_loss = -(0 + yt) * K.log(0 + yp + K.epsilon())
     neg_loss = -(1 - yt) * K.log(1 - yp + K.epsilon())
