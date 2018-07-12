d32fb6fee351bbcf3a8cb3506a82c3e2ebf7f334
Matthew Scholefield <matthew331199@gmail.com>
2018-07-12 14:21:54 -0500

    Fix extra comma on assignment

--- precise/train_data.py
+++ precise/train_data.py
@@ -205,7 +205,7 @@ class TrainData:
         add(nkw_files, 0.0)
 
         from precise.params import pr
-        inputs = np.array(inputs) if inputs else np.empty((0, pr.n_features, pr.feature_size)),
+        inputs = np.array(inputs) if inputs else np.empty((0, pr.n_features, pr.feature_size))
         outputs = np.array(outputs) if outputs else np.empty((0, 1))
         shuffle_ids = np.arange(len(inputs))
         np.random.shuffle(shuffle_ids)
