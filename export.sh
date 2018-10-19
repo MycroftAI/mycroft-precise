e3c74dd44dd2de02967771303dd43f531239b1d5
Matthew Scholefield <matthew331199@gmail.com>
2018-10-19 12:55:31 -0500

    Improve export script

--- export.sh
+++ export.sh
@@ -13,30 +13,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if ! [[ "$1" =~ .*\.net$ ]] || ! [ -f "$1" ] || [ "$#" != "1" ]; then
-	echo "Usage: $0 <model>.net"
+if ! [[ "$1" =~ .*\.net$ ]] || ! [ -f "$1" ] || ! [[ $# =~ [2-3] ]]; then
+	echo "Usage: $0 <model>.net GITHUB_REPO [BRANCH]"
 	exit 1
 fi
 
 model_file=$(readlink -f "$1")
+repo=$2
+branch=${3-master}
+
 cd "$(dirname "$0")"
 set -e
-cache=.cache/precise-data
-[ -d "$cache" ] || git clone https://github.com/mycroftai/precise-data "$cache" -b models --single-branch
+cache=.cache/precise-data/${repo//\//.}.${branch//\//.}
+[ -d "$cache" ] || git clone "$repo" "$cache" -b "$branch" --single-branch
 
 pushd "$cache"
 git fetch
-git checkout models
-git reset --hard origin/models
+git checkout "$branch"
+git reset --hard "origin/$branch"
 popd
 
 source .venv/bin/activate
 model_name=$(basename "${1%%.net}")
 precise-convert "$model_file" -o "$cache/$model_name.pb"
-read -p "Uploading $model_file to public repo with name \"$model_name\". Confirm? (y/n) " response
-if [ "$response" != "y" ] && [ "$response" != "Y" ]; then
-    return 1
-fi
 
 pushd "$cache"
 tar cvf "$model_name.tar.gz" "$model_name.pb" "$model_name.pb.params"
@@ -44,6 +43,17 @@ md5sum "$model_name.tar.gz" > "$model_name.tar.gz.md5"
 rm -f "$model_name.pb" "$model_name.pb.params" "$model_name.pbtxt"
 git reset
 git add "$model_name.tar.gz" "$model_name.tar.gz.md5"
+
+echo
+ls
+git status
+
+read -p "Uploading $model_name model to branch $branch on repo $repo. Confirm? (y/N) " answer
+if [ "$answer" != "y" ] && [ "$answer" != "Y" ]; then
+    echo "Aborted."
+    exit 1
+fi
+
 git commit -m "Update $model_name"
 git push
 popd
