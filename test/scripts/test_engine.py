6196bdca26bf3ee279a8cdadb696a5c0e776afa5
Matthew D. Scholefield <matthew331199@gmail.com>
2019-11-01 01:17:31 -0500

    Add a series of test cases using the new api
    The primary purpose is to ensure all the scripts run end to end rather than completely verifying its functionality

---
+++ test/scripts/test_engine.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# Copyright 2019 Mycroft AI Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import sys
+
+import glob
+import re
+from os.path import join
+
+from precise.scripts.engine import EngineScript
+from runner.precise_runner import ReadWriteStream
+
+
+class FakeStdin:
+    def __init__(self, data: bytes):
+        self.buffer = ReadWriteStream(data)
+
+    def isatty(self):
+        return False
+
+
+class FakeStdout:
+    def __init__(self):
+        self.buffer = ReadWriteStream()
+
+
+def test_engine(train_folder, train_script):
+    train_script.run()
+    with open(glob.glob(join(train_folder.root, 'wake-word', '*.wav'))[0], 'rb') as f:
+        data = f.read()
+    try:
+        sys.stdin = FakeStdin(data)
+        sys.stdout = FakeStdout()
+        EngineScript.create(model_name=train_folder.model).run()
+        assert re.match(rb'[01]\.[0-9]+', sys.stdout.buffer.buffer)
+    finally:
+        sys.stdin = sys.__stdin__
+        sys.stdout = sys.__stdout__
---
+++ test/scripts/test_train.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# Copyright 2019 Mycroft AI Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from os.path import isfile
+
+from precise.params import pr
+from precise.scripts.train import TrainScript
+from test.scripts.dummy_audio_folder import DummyAudioFolder
+
+
+class DummyTrainFolder(DummyAudioFolder):
+    def __init__(self, count=10):
+        super().__init__(count)
+        self.generate_samples(self.subdir('wake-word'), 'ww-{}.wav', 1.0, self.rand(0, 2 * pr.buffer_t))
+        self.generate_samples(self.subdir('not-wake-word'), 'nww-{}.wav', 0.0, self.rand(0, 2 * pr.buffer_t))
+        self.generate_samples(self.subdir('test', 'wake-word'), 'ww-{}.wav', 1.0, self.rand(0, 2 * pr.buffer_t))
+        self.generate_samples(self.subdir('test', 'not-wake-word'), 'nww-{}.wav', 0.0, self.rand(0, 2 * pr.buffer_t))
+        self.model = self.path('model.net')
+
+
+class TestTrain:
+    def test_run_basic(self):
+        folders = DummyTrainFolder(10)
+        script = TrainScript.create(model=folders.model, folder=folders.root)
+        script.run()
+        assert isfile(folders.model)
