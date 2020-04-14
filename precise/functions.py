af0f59859030fbab3dea13e775892ac4241afb3c
Chris Veilleux <veilleux.chris@gmail.com>
2020-04-14 15:55:27 -0500

    added fetch before diff #27

--- precise/functions.py
+++ precise/functions.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""This is another test."""
 from math import exp, log, sqrt, pi
 import numpy as np
 from typing import *
