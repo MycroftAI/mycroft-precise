2b60e94820f0f3184f4206ae3c82820e0d5c0d6b
NoamDev <37066741+NoamDev@users.noreply.github.com>
2020-09-22 15:56:19 +0300

    make sure batch scripts exit with 1 on error

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-call setup.bat || exit /b
-call .venv/Scripts/activate || exit /b
-pip install pyinstaller || exit /b
-python win-package.py || exit /b
+call setup.bat || exit /b 1
+call .venv/Scripts/activate || exit /b 1
+pip install pyinstaller || exit /b 1
+python win-package.py || exit /b 1
--- setup.bat
+++ setup.bat
@@ -1,9 +1,9 @@
 @echo off
 if not exist ".venv" (
-	py -3.6 -m venv .venv || exit /b
+	py -3.6 -m venv .venv || exit /b 1
 )
-call .venv/Scripts/activate || exit /b
-pip install -e runner/ || exit /b
+call .venv/Scripts/activate || exit /b 1
+pip install -e runner/ || exit /b 1
 pip install -e . || exit /b
 rem  Optional, for comparison
-pip install pocketsphinx || exit /b
+pip install pocketsphinx || exit /b 1
